440059f8f98947288b308b5a9880d09e1baa61be
Shreya <123@gmail.com>
2021-03-23 18:55:46 -0500

    Bell Icon

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,31 +1,55 @@
 import React, { Component} from 'react';
 import { Header,Icon,Badge } from 'react-native-elements';
-import { View, Text, StyeSheet ,Alert} from 'react-native';
+import { View} from 'react-native';
+import db from "../config";
 
+export default class MyHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
 
+  bellIcon = () => {
+    return(
+      <View>
+        <Icon name = "bell" type = "font-awesome" color = "696969" size = {25} onPress = {() => {
+          this.props.navigation.navigate("Notification");
+        }}/>
+  
+        <Badge value = {this.state.value} containerStyle = {{position: 'absolute', right: -4, top: -4}}/>
+      </View>
+    );
+  }
 
-const BellIconWithBadge=(props)=>{
-  return(
-    <View>
-      <Icon name='bell' type='font-awesome' color='#696969' size={25}
-        onPress={() =>props.navigation.navigate('Notification')}/>
-       <Badge
-        value="1"
-       containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
-    </View>
-  )
-}
+  getNumberOfUnreadNotifications() {
+    db.collection("all_notifications").where("notification_status", "==", "unread")
+    .onSnapshot((snapshot) => {
+      var unreadNotifications = snapshot.docs.map((doc) => {
+        doc.data();
+      });
+      this.setState({
+        value: unreadNotifications.length
+      });
+    })
+  }
 
+  componentDidMount() {
+    this.getNumberOfUnreadNotifications();
+  }
 
-const MyHeader = props => {
-  return (
-    <Header
-      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
-      centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
-      rightComponent={<BellIconWithBadge {...props}/>}
-      backgroundColor = "#eaf8fe"
-    />
-  );
+  render() {
+    return (
+      <Header
+        leftComponent = {<Icon name = "bars" type = "font-awesome" color = "#696969" onPress = {() => {
+          this.props.navigation.toggleDrawer();
+        }}/>}
+        centerComponent = {{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+        rightComponent = {<this.bellIcon {...this.props}/>}
+        backgroundColor = "#eaf8fe"
+      />
+    );
+  }
 };
 
-export default MyHeader;
--- config.js
+++ config.js
@@ -2,15 +2,13 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyASyOjOtJla-X-b3io2eLoaUc_bIRFSIIc",
-    authDomain: "book-santa-app.firebaseapp.com",
-    databaseURL: "https://book-santa-app.firebaseio.com",
-    projectId: "book-santa-app",
-    storageBucket: "book-santa-app.appspot.com",
-    messagingSenderId: "69634746716",
-    appId: "1:69634746716:web:6fbbfc110fb4475365f999",
-    measurementId: "G-DLB7XC0JPL"
-  };
+  apiKey: "AIzaSyBlSlHUDNofnmcuBEguEPjETt7VKLG-ZCI",
+  authDomain: "booksanta-93237.firebaseapp.com",
+  projectId: "booksanta-93237",
+  storageBucket: "booksanta-93237.appspot.com",
+  messagingSenderId: "522882045633",
+  appId: "1:522882045633:web:f168ebf85113b0f973e640"
+};
 
 
 // Initialize Firebase
